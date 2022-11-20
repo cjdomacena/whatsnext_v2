5fd29a5ad52a9105f8e50c8ff821f282393d8fd2
cjdomacena <cjdomacena@yahoo.com>
2022-11-20 13:41:54 -0500

    ui: Added Header comp

---
+++ components/MetaHeader.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+type HeaderProps = {
+  title: string;
+  description: string;
+};
+
+const MetaHeader: React.FC<HeaderProps> = (props) => {
+  return (
+    <Head>
+      <title>{props.title}</title>
+      <meta name="description" content={props.description} />
+      <meta property="og:url" content={""} />
+      <meta property="og:title" content={props.title} />
+      <meta property="og:description" content={props.description} />
+      <meta property="og:site_name" content="What's Next" />
+    </Head>
+  );
+};
+export default MetaHeader;
