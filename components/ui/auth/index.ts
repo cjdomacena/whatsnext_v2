e2e6f1bb0d0b0d932702491101ff272688cf01e3
cjdomacena <cjdomacena@yahoo.com>
2022-11-20 13:41:19 -0500

    updated imports

--- components/ui/auth/index.ts
+++ components/ui/auth/index.ts
@@ -1,2 +1,3 @@
-export { default } from "./Register";
+export { default as Register } from "./Register";
 export { default as Login } from "./Login";
+export { Button, CancelButton } from "./Button";
