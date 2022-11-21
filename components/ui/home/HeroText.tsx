e43c025221482ed35684311ee1dfc551a66c7151
cjdomacena <cjdomacena@yahoo.com>
2022-11-21 18:18:46 -0500

    ui: bg change

--- components/ui/home/HeroText.tsx
+++ components/ui/home/HeroText.tsx
@@ -1,10 +1,16 @@
+import { TbClick } from "react-icons/tb";
 const HeroText: React.FC<any> = () => {
   return (
     <div className=" w-full">
       <div className="container mx-auto rounded relative text-center leading-loose">
-        <h1 className=" uppercase -tracking-wider font-black whitespace-wrap  bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent 2xl:text-9xl  xl:text-9xl  lg:text-9xl  md:text-8xl text-6xl ">
-          Life is too short for ordinary apps
-        </h1>
+        <h4 className="text-4xl font-bold">WE ARE</h4>
+        <div className="relative">
+          <h1 className=" uppercase -tracking-wider font-black whitespace-wrap  bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent 2xl:text-9xl  xl:text-9xl  lg:text-9xl  md:text-8xl text-6xl ">
+            WATCHFLIX
+          </h1>
+          <TbClick className="w-6 h-6 absolute top-0 right-0" />
+        </div>
+        <div className="w-full h-52 bg-neutral-900"></div>
       </div>
     </div>
   );
