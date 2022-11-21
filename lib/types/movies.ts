e9419c31aac588681f7c45ad1f1ede4e0e6b005b
cjdomacena <cjdomacena@yahoo.com>
2022-11-20 19:33:28 -0500

    chore: added endpoint and type for trending movies

---
+++ lib/types/movies.ts
@@ -0,0 +1,17 @@
+export interface TrendingMovie {
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  title: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date: Date;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
---
+++ pages/api/trending/movie.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const data = await fetch(
+      `${process.env.TMDB_URL}/trending/movie/week?api_key=${process.env.TMDB_API_KEY}`
+    );
+    const results = await data.json();
+    if (results.hasOwnProperty("success") && !results.success) {
+      throw new Error(results.status_message);
+    }
+    return res.status(200).json(results);
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(500).json({ error: e.message });
+    }
+    return res.status(500).json({ error: "Something went wrong..." });
+  }
+}
