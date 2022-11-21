71d2a39ee185869f4c20b9d131f9ee91bdefd632
cjdomacena <cjdomacena@yahoo.com>
2022-11-20 19:34:03 -0500

    wip

---
+++ lib/api/getTrendingMovie.ts
@@ -0,0 +1,14 @@
+export const getTrendingMovie = async () => {
+  try {
+    const req = await fetch("/api/trending/movie");
+    const res = await req.json();
+
+    if (res.hasOwnProperty("error")) {
+      throw new Error(res.error);
+    }
+
+    return res;
+  } catch (e) {
+    alert(e);
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import HeroText from "@components/ui/home/HeroText";
+import { getTrendingMovie } from "@lib/api/getTrendingMovie";
+import { QUERY_CONFIG } from "@lib/constants/config";
+import { QueryResult } from "@lib/types/common";
+import { TrendingMovie } from "@lib/types/movies";
+import { useQuery } from "@tanstack/react-query";
+import { NextPage } from "next";
 
-export default function Home() {
+const Home: NextPage = () => {
+  const { data, status } = useQuery<QueryResult<TrendingMovie>, Error>(
+    ["trending", "movie"],
+    getTrendingMovie,
+    { ...QUERY_CONFIG }
+  );
+
   return (
     <div>
       <HeroText />
     </div>
   );
-}
+};
+
+export default Home;
