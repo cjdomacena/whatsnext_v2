6e0969fabc6a805eaf7656a2b2bf2007384cc487
cjdomacena <cjdomacena@yahoo.com>
2022-11-24 14:21:00 -0500

    removed console logs

--- lib/api/getTrendingMovie.ts
+++ lib/api/getTrendingMovie.ts
@@ -1,9 +1,6 @@
-import { BASE_URL } from "@lib/constants/config";
-
 export const getTrendingMovie = async () => {
-  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
   try {
-    const req = await fetch(`${BASE_URL}/api/trending/movie`);
+    const req = await fetch(`/api/trending/movie`);
     const res = await req.json();
 
     if (res.hasOwnProperty("error")) {
