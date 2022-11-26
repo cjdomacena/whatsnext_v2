c7ce764d78fd23cd5022795789f5e9ed34369fee
cjdomacena <cjdomacena@yahoo.com>
2022-11-25 20:36:44 -0500

    chore: automatically update table to add customer id

--- pages/api/stripe/create-customer.ts
+++ pages/api/stripe/create-customer.ts
@@ -1,36 +1,43 @@
-import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { createClient } from "@supabase/supabase-js";
 import { NextApiRequest, NextApiResponse } from "next/types";
 import Stripe from "stripe";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const supabase = createServerSupabaseClient({ req, res });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-  const token = req.cookies["supabase-auth-token"];
-  if (token && session?.user) {
-    try {
-      const stripe = new Stripe(process.env.STRIPE_KEY as string, {
-        apiVersion: "2022-11-15",
-      });
-      const { data } = await stripe.customers.search({
-        query: `email:\'xjohnx@testing.com\'`,
-      });
-
-      if (data.length === 0) {
-        const newUser = await stripe.customers.create({
-          description: "Customer created from api!",
-          name: session.user.user_metadata.full_name,
-          email: session.user.email,
-        });
-        return res.status(200).json({ userId: newUser.id });
-      }
-      return res.status(200).json({ message: "User already registered" });
-    } catch (e) {
-      return res.status(400).json(e);
-    }
+  if (req.query.API_ROUTE_SECRET !== process.env.API_ROUTE_SECRET) {
+    return res.status(401).json("Request not authorized.");
   }
-  res.status(403).json("Permission Denied");
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.SERVICE_KEY as string
+  );
+  try {
+    const stripe = new Stripe(process.env.STRIPE_KEY as string, {
+      apiVersion: "2022-11-15",
+    });
+    const { data } = await stripe.customers.search({
+      query: `email:\'xjohnx@testing.com\'`,
+    });
+    if (data.length === 0) {
+      const customer = await stripe.customers.create({
+        description: "Customer created from api!",
+        name: req.body.record.full_name,
+        email: req.body.record.email,
+      });
+      if (customer) {
+        await supabase
+          .from("profiles")
+          .update({
+            stripe_id: customer.id,
+          })
+          .eq("id", req.body.record.id);
+      }
+
+      return res.status(200).json({ userId: req.body.record.id });
+    }
+    return res.status(200).json({ message: "User already registered" });
+  } catch (e) {
+    return res.status(400).json(e);
+  }
 }
