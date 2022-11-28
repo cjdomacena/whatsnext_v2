d404e0dfb01bc427814a0d369d53204be7aa5ad1
cjdomacena <cjdomacena@yahoo.com>
2022-11-27 20:50:21 -0500

    updated session handler supabase on server

--- pages/api/stripe/checkout-session.ts
+++ pages/api/stripe/checkout-session.ts
@@ -9,32 +9,41 @@ export default async function handler(
   const {
     data: { session },
   } = await supabase.auth.getSession();
-  const token = req.cookies["supabase-auth-token"];
-  if (token && session?.user) {
-    try {
-      const stripe = new Stripe(process.env.STRIPE_KEY as string, {
-        apiVersion: "2022-11-15",
-      });
-      // const customer = stripe.customers.retrieve()
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: "price_1M6jfyEbEpPvaCtH6HvxlBOm",
-            quantity: 1,
-          },
-        ],
-        mode: "subscription",
-        success_url: `http://localhost:3000/auth/checkout?success=true&session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `http://localhost:3000/auth/checkout?success=false&session_id={CHECKOUT_SESSION_ID}`,
-        payment_method_types: ["card"],
-      });
-      if (session) {
-        return res.redirect(303, session.url as string);
+  if (session) {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("stripe_id")
+      .eq("id", session.user.id)
+      .single();
+
+    if (data) {
+      try {
+        const stripe = new Stripe(process.env.STRIPE_KEY as string, {
+          apiVersion: "2022-11-15",
+        });
+        // const customer = stripe.customers.retrieve()
+        const checkoutSession = await stripe.checkout.sessions.create({
+          customer: data?.stripe_id,
+          line_items: [
+            {
+              price: "price_1M6jfyEbEpPvaCtH6HvxlBOm",
+              quantity: 1,
+            },
+          ],
+          mode: "subscription",
+          success_url: `http://localhost:3000/payment/success`,
+          cancel_url: `http://localhost:3000/payment/failed`,
+          payment_method_types: ["card"],
+        });
+        if (session) {
+          return res.redirect(303, checkoutSession.url as string);
+        }
+        return res.status(200).json({ message: "Something went wrong.." });
+      } catch (e) {
+        return res.status(400).json(e);
       }
-      return res.status(200).json({ message: "Something went wrong.." });
-    } catch (e) {
-      return res.status(400).json(e);
     }
+    return res.status(401).send(`Something went wrong: ${error.message}`);
   }
   res.status(403).json("Permission Denied");
 }
