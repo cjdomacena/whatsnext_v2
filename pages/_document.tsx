4627a0590876b509172f1020b9d17d9a05c16cee
cjdomacena <cjdomacena@yahoo.com>
2022-12-04 23:37:12 -0500

    updated path for favicos

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,20 +8,20 @@ export default function Document() {
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="favico/favicon-32x32.png"
+          href="/favico/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="favico/favicon-16x16.png"
+          href="/favico/favicon-16x16.png"
         />
-        <link rel="apple-touch-icon" href="favico/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/favico/apple-touch-icon.png" />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="favico/android-chrome-192x192.png"
+          href="/favico/android-chrome-192x192.png"
         />
       </Head>
       <body>
