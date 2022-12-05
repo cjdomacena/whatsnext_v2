f7df829d1e500b872c13dc4d90081e89477ff0c6
cjdomacena <cjdomacena@yahoo.com>
2022-12-05 12:37:52 -0500

    added page loader for ssr pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import "@fontsource/work-sans/700.css";
 import "@fontsource/work-sans/800.css";
 import "@fontsource/work-sans/900.css";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import MetaHeader from "@components/MetaHeader";
@@ -19,23 +19,48 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import "keen-slider/keen-slider.min.css";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import Footer from "@components/common/footer";
+import { Router } from "next/router";
 
 export default function App({ Component, pageProps, ...appProps }: AppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   const [queryClient] = useState(() => new QueryClient());
 
+  useEffect(() => {
+    const routeEventStart = (url: string) => {
+      console.log(url);
+      if (url.includes("/details")) {
+        toast.loading("Loading...", {
+          position: "top-right",
+        });
+      }
+    };
+    const routeEventEnd = (url: string) => {
+      if (url.includes("/details")) {
+        toast.dismiss();
+      }
+    };
+
+    Router.events.on("routeChangeStart", routeEventStart);
+    Router.events.on("routeChangeComplete", routeEventEnd);
+    Router.events.on("routeChangeError", routeEventEnd);
+    return () => {
+      Router.events.off("routeChangeStart", routeEventStart);
+      Router.events.off("routeChangeComplete", routeEventEnd);
+      Router.events.off("routeChangeError", routeEventEnd);
+    };
+  }, []);
   return (
     <main className="min-w-screen transition-colors overflow-x-hidden">
+      <MetaHeader
+        title="WhatsNext"
+        description="Pick up where you left off. Track your shows!"
+      />
       <SessionContextProvider
         supabaseClient={supabaseClient}
         initialSession={pageProps.initialSession}
       >
-        <MetaHeader
-          title="WhatsNext"
-          description="Pick up where you left off. Track your shows!"
-        />
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
             {appProps.router.pathname.split("/").includes("auth") ? null : (
