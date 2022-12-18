595e6cdf1d703fcaca1f690149241f00c160a7da
cjdomacena <cjdomacena@yahoo.com>
2022-12-17 23:57:09 -0500

    fix

--- lib/api/getVerifiedReview.ts
+++ lib/api/getVerifiedReview.ts
@@ -5,11 +5,12 @@ export const getVerifiedReview = async (
   media_type: string,
   supabase: SupabaseClient
 ) => {
+  const ids = [436270, 119051, 12345];
   const { data, error } = await supabase
     .from("featured_reviews")
     .select("*, profiles(full_name)")
     .eq("media_type", "movie")
-    .eq("movie_id", 12345)
+    .eq("movie_id", ids[Math.floor(Math.random() * 3)])
     .single();
   if (error) {
     if (error.code === "PGRST116") {
