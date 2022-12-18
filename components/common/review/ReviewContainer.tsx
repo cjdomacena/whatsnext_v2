c1f8278164070a3f6d0a6b443f7194bee8322129
cjdomacena <cjdomacena@yahoo.com>
2022-12-17 23:29:30 -0500

    fix: not matching initial render

--- components/common/review/ReviewContainer.tsx
+++ components/common/review/ReviewContainer.tsx
@@ -3,6 +3,7 @@ import { IReview } from "@lib/types/supabase/database";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { PostgrestError } from "@supabase/supabase-js";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import Loader from "./Loader";
 import Review from "./Review";
 
@@ -12,14 +13,27 @@ type CommentProps = {
 
 const ReviewContainer: React.FC<CommentProps> = ({ movie_id }) => {
   const supabase = useSupabaseClient();
+  const [isHydrated, setIsHydrated] = useState<boolean>(false);
 
-  const { data: reviews, error } = useQuery<IReview[], PostgrestError | Error>(
+  const {
+    data: reviews,
+    error,
+    refetch,
+  } = useQuery<IReview[], PostgrestError | Error>(
     ["reviews", { id: movie_id }],
     async () => getReviews(movie_id as string, supabase),
     {
+      enabled: false,
       refetchOnWindowFocus: true,
     }
   );
+
+  useEffect(() => {
+    if (!isHydrated) {
+      refetch();
+      setIsHydrated(true);
+    }
+  }, [isHydrated, refetch]);
 
   if (error) {
     return <h4>Something went wrong...</h4>;
