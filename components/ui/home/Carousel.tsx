0e638bb6226ec6ff453539fdb769e3f81b718e92
cjdomacena <cjdomacena@yahoo.com>
2022-12-17 21:30:37 -0500

    feat: added button for navigating slideshow

--- components/ui/home/Carousel.tsx
+++ components/ui/home/Carousel.tsx
@@ -2,14 +2,17 @@ import { IQueryResult } from "@lib/types/movies.type";
 import CarouselSlide from "./CarouselSlide";
 import { useKeenSlider } from "keen-slider/react";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import { IoArrowBack, IoArrowForward } from "react-icons/io5";
 type CarouselProps = {
   data: IQueryResult[];
   media: "tv" | "movie";
 };
 const Carousel: React.FC<CarouselProps> = ({ data, media }) => {
-  const [ref] = useKeenSlider<HTMLDivElement>({
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const [ref, instanceRef] = useKeenSlider<HTMLDivElement>({
     initial: 0,
-    loop: false,
+    loop: true,
     mode: "snap",
     breakpoints: {
       // 400px and up
@@ -36,18 +39,42 @@ const Carousel: React.FC<CarouselProps> = ({ data, media }) => {
       perView: 1,
       spacing: 15,
     },
+    created() {
+      setLoaded(true);
+    },
   });
   return (
-    <motion.div
-      ref={ref}
-      className="w-full keen-slider overflow-x-hidden"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-    >
-      {data.map((movie) => (
-        <CarouselSlide key={movie.id} movie={movie} media={media} />
-      ))}
-    </motion.div>
+    <>
+      {loaded && instanceRef.current ? (
+        <div className="w-fit ml-auto space-x-2">
+          <button
+            onClick={(e: any) =>
+              e.stopPropagation() || instanceRef.current?.prev()
+            }
+          >
+            <IoArrowBack />
+          </button>
+          <button
+            onClick={(e: any) =>
+              e.stopPropagation() || instanceRef.current?.next()
+            }
+          >
+            <IoArrowForward />
+          </button>
+        </div>
+      ) : null}
+
+      <motion.div
+        ref={ref}
+        className="w-full keen-slider overflow-x-hidden relative"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
+        {data.map((movie) => (
+          <CarouselSlide key={movie.id} movie={movie} media={media} />
+        ))}
+      </motion.div>
+    </>
   );
 };
 
