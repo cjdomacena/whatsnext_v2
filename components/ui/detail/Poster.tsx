0b05e3dd00b7e81a3c4ff41a0457528002cc3488
cjdomacena <cjdomacena@yahoo.com>
2023-02-27 16:39:00 -0500

    chore: removed image optimization

--- components/ui/detail/Poster.tsx
+++ components/ui/detail/Poster.tsx
@@ -1,5 +1,4 @@
 import { IMAGE_URL } from "@lib/constants/config";
-import Image from "next/image";
 type PosterProps = {
   posterPath: string;
 };
@@ -7,19 +6,19 @@ type PosterProps = {
 const Poster: React.FC<PosterProps> = ({ posterPath }) => {
   return (
     <div className="w-[300px] h-[450px] relative">
-      <Image
+      <img
         src={`${
           posterPath
-            ? IMAGE_URL + "/w300_and_h450_bestv2" + posterPath
+            ? IMAGE_URL +
+              "/w300_and_h450_bestv2" +
+              posterPath
             : "/assets/fallback.png"
         }`}
-        fill
         alt=""
-        sizes="(max-width: 768px) 50vw,
-      (max-width: 1200px) 50vw,
-     50vw"
+        //     sizes="(max-width: 768px) 50vw,
+        //   (max-width: 1200px) 50vw,
+        //  50vw"
         className="rounded  dark:drop-shadow-[0_5px_35px_rgba(32,32,32,0.6)] drop-shadow-2xl"
-        priority={true}
       />
     </div>
   );
